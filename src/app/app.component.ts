66038d9e76836864faa4ab8e526c48a1655dbde3
Geomitron <22552797+Geomitron@users.noreply.github.com>
2020-02-02 19:26:14 -0500

    Initial Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -28,5 +28,5 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'NewBridge';
+  title = 'NewBridge'
 }
