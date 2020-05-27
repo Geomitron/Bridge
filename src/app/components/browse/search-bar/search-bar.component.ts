db7bb8ec20ee9e1914aff41077bba1f039b28644
Geomitron <22552797+Geomitron@users.noreply.github.com>
2020-05-27 15:07:31 -0400

    Added database connection error handling

--- src/app/components/browse/search-bar/search-bar.component.ts
+++ src/app/components/browse/search-bar/search-bar.component.ts
@@ -8,13 +8,25 @@ import { SearchService } from 'src/app/core/services/search.service'
 })
 export class SearchBarComponent implements AfterViewInit {
 
+  isError = false
+
   constructor(public searchService: SearchService) { }
 
   ngAfterViewInit() {
     $('.ui.dropdown').dropdown()
+    $('#searchIcon').popup({
+      onShow: () => this.isError // Only show the popup if there is an error
+    })
+    this.searchService.onSearchError(() => {
+      this.isError = true
+    })
   }
 
   onSearch(query: string) {
     this.searchService.newSearch(query)
+  }
+
+  isLoading() {
+    return this.searchService.isLoading()
   }
 }
