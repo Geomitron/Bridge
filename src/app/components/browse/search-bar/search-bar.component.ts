10533edb78e7ffd501c49d12339debd23c79162c
Geomitron <22552797+Geomitron@users.noreply.github.com>
2021-03-30 18:21:55 -0500

    Advanced Search HTML/CSS

--- src/app/components/browse/search-bar/search-bar.component.ts
+++ src/app/components/browse/search-bar/search-bar.component.ts
@@ -11,11 +11,13 @@ export class SearchBarComponent implements AfterViewInit {
   @ViewChild('searchIcon', { static: true }) searchIcon: ElementRef
 
   isError = false
+  showAdvanced = false
 
   constructor(public searchService: SearchService) { }
 
   ngAfterViewInit() {
     $('.ui.dropdown').dropdown()
+    $('.ui.range.slider').slider({ min: 0, max: 6, start: 0, end: 6, step: 1 })
     $(this.searchIcon.nativeElement).popup({
       onShow: () => this.isError // Only show the popup if there is an error
     })
@@ -26,6 +28,10 @@ export class SearchBarComponent implements AfterViewInit {
 
   onSearch(query: string) {
     this.searchService.newSearch(query)
+  }
+
+  onAdvancedSearchClick() {
+    this.showAdvanced = !this.showAdvanced
   }
 
   isLoading() {
