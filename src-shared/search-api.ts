0014206988d607c90ba91dc9fa752ed719299a26
Geomitron <22552797+Geomitron@users.noreply.github.com>
2024-07-29 16:04:57 -0500

    Update search API

--- src-shared/search-api.ts
+++ src-shared/search-api.ts
@@ -8,9 +8,10 @@ export const GeneralSearchSchema = z.object({
 	search: z.string(),
 	per_page: z.number().positive().lte(250, 'Getting more than 250 results at a time is not supported').optional(),
 	page: z.number().positive(),
-	instrument: z.enum(instruments).nullable(),
-	difficulty: z.enum(difficulties).nullable(),
-	drumType: z.enum(drumTypeNames).nullable(),
+	instrument: z.enum(instruments).nullable().default(null),
+	difficulty: z.enum(difficulties).nullable().default(null),
+	drumType: z.enum(drumTypeNames).nullable().default(null),
+	source: z.enum(sources).optional(),
 })
 export type GeneralSearch = z.infer<typeof GeneralSearchSchema>
 
@@ -18,49 +19,61 @@ const md5Validator = z.string().regex(/^[a-f0-9]{32}$/, 'Invalid MD5 hash')
 const blakeValidator = z.string().regex(/^[A-Za-z0-9-_]+={0,2}$/, 'Invalid hash')
 
 export const AdvancedSearchSchema = z.object({
-	instrument: z.enum(instruments).nullable(),
-	difficulty: z.enum(difficulties).nullable(),
-	drumType: z.enum(drumTypeNames).nullable(),
+	instrument: z.enum(instruments).nullable().default(null),
+	difficulty: z.enum(difficulties).nullable().default(null),
+	drumType: z.enum(drumTypeNames).nullable().default(null),
 	source: z.enum(sources).optional(),
 	per_page: z.number().positive().lte(250, 'Getting more than 250 results at a time is not supported').optional(),
 	page: z.number().positive().optional(),
-	name: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }),
-	artist: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }),
-	album: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }),
-	genre: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }),
-	year: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }),
-	charter: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }),
-	minLength: z.number().nullable(),
-	maxLength: z.number().nullable(),
-	minIntensity: z.number().nullable(),
-	maxIntensity: z.number().nullable(),
-	minAverageNPS: z.number().nullable(),
-	maxAverageNPS: z.number().nullable(),
-	minMaxNPS: z.number().nullable(),
-	maxMaxNPS: z.number().nullable(),
-	minYear: z.number().nullable(),
-	maxYear: z.number().nullable(),
-	modifiedAfter: z.string().regex(/^\d+-\d{2}-\d{2}$/, 'Invalid date').or(z.coerce.date()).or(z.literal('')).nullable(),
-	hash: z.string().transform(data =>
-		data === '' || data.split(',').every(hash => md5Validator.safeParse(hash).success) ? data : 'invalid'
-	).nullable(),
-	chartHash: z.string().transform(data =>
-		data === '' || data.split(',').every(hash => md5Validator.safeParse(hash).success) ? data : 'invalid'
-	).nullable().optional(),
-	trackHash: z.string().transform(data =>
-		data === '' || data.split(',').every(hash => blakeValidator.safeParse(hash).success) ? data : 'invalid'
-	).nullable().optional(),
-	hasSoloSections: z.boolean().nullable(),
-	hasForcedNotes: z.boolean().nullable(),
-	hasOpenNotes: z.boolean().nullable(),
-	hasTapNotes: z.boolean().nullable(),
-	hasLyrics: z.boolean().nullable(),
-	hasVocals: z.boolean().nullable(),
-	hasRollLanes: z.boolean().nullable(),
-	has2xKick: z.boolean().nullable(),
-	hasIssues: z.boolean().nullable(),
-	hasVideoBackground: z.boolean().nullable(),
-	modchart: z.boolean().nullable(),
+	name: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }).nullable().default(null),
+	artist: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }).nullable().default(null),
+	album: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }).nullable().default(null),
+	genre: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }).nullable().default(null),
+	year: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }).nullable().default(null),
+	charter: z.object({ value: z.string(), exact: z.boolean(), exclude: z.boolean() }).nullable().default(null),
+	minLength: z.number().nullable().default(null),
+	maxLength: z.number().nullable().default(null),
+	minIntensity: z.number().nullable().default(null),
+	maxIntensity: z.number().nullable().default(null),
+	minAverageNPS: z.number().nullable().default(null),
+	maxAverageNPS: z.number().nullable().default(null),
+	minMaxNPS: z.number().nullable().default(null),
+	maxMaxNPS: z.number().nullable().default(null),
+	minYear: z.number().nullable().default(null),
+	maxYear: z.number().nullable().default(null),
+	modifiedAfter: z
+		.string()
+		.regex(/^\d+-\d{2}-\d{2}$/, 'Invalid date')
+		.or(z.coerce.date())
+		.or(z.literal(''))
+		.nullable()
+		.default(null),
+	hash: z
+		.string()
+		.transform(data => (data === '' || data.split(',').every(hash => md5Validator.safeParse(hash).success) ? data : 'invalid'))
+		.nullable()
+		.default(null),
+	chartHash: z
+		.string()
+		.transform(data => (data === '' || data.split(',').every(hash => md5Validator.safeParse(hash).success) ? data : 'invalid'))
+		.nullable()
+		.optional(),
+	trackHash: z
+		.string()
+		.transform(data => (data === '' || data.split(',').every(hash => blakeValidator.safeParse(hash).success) ? data : 'invalid'))
+		.nullable()
+		.optional(),
+	hasSoloSections: z.boolean().nullable().default(null),
+	hasForcedNotes: z.boolean().nullable().default(null),
+	hasOpenNotes: z.boolean().nullable().default(null),
+	hasTapNotes: z.boolean().nullable().default(null),
+	hasLyrics: z.boolean().nullable().default(null),
+	hasVocals: z.boolean().nullable().default(null),
+	hasRollLanes: z.boolean().nullable().default(null),
+	has2xKick: z.boolean().nullable().default(null),
+	hasIssues: z.boolean().nullable().default(null),
+	hasVideoBackground: z.boolean().nullable().default(null),
+	modchart: z.boolean().nullable().default(null),
 	chartIdAfter: z.number().positive().optional(),
 })
 export type AdvancedSearch = z.infer<typeof AdvancedSearchSchema>
