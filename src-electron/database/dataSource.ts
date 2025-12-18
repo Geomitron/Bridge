59d57bb7eb11491a6d46d4eafb000cca659d7ba3
Myx <ludvig_olausson@hotmail.com>
2025-12-18 03:10:08 +0100

    Update sql driver

--- src-electron/database/dataSource.ts
+++ src-electron/database/dataSource.ts
@@ -1,13 +1,17 @@
 import { DataSource } from 'typeorm'
+
 import { Chart } from './entities/Chart.js'
 import { Init1743124434920 } from './migrations/1743124434920-init.js'
 
 const migrations = [Init1743124434920]
 const entities = [Chart]
+let database: Uint8Array | undefined
 
 export const dataSource = new DataSource({
-	type: "sqlite",
-	database: "library.sqlite",
+	type: "sqljs",
+	database: database,
+	location: "library.sqlite",
+	autoSave: true,
 	entities: entities,
 	// Configure migrations to use a folder that contains your migration files:
 	migrations: migrations,
